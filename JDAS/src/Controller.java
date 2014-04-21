f069805e6457908fafd7587dff733e52da0f9e03
leeyimin <imleeyimin@gmail.com>
2014-04-22 00:51:58 +0800

    adding con and mod

---
+++ JDAS/src/Controller.java
@@ -0,0 +1,54 @@
+
+import java.io.*;
+import java.util.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class Controller {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.1F142A4E-D39E-0BD4-1AFB-745615ACC24F]
+    // </editor-fold> 
+    public Controller () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.B60F2BDE-46D1-8EA8-6203-F46C3D0AB2D0]
+    // </editor-fold> 
+    public void uploadPublication (File pFile) {
+        
+    }
+    
+    public ArrayList<ArrayList<String> > parseCSV (File cFile) throws FileNotFoundException{
+        ArrayList<ArrayList<String> > res = new ArrayList<>();
+        Scanner sc= new Scanner(cFile);
+        StringTokenizer st;
+        ArrayList<String> list;
+        while(sc.hasNextLine()){
+            st=new StringTokenizer(sc.nextLine(),",");
+            list= new ArrayList<>();
+            while(st.hasMoreTokens()){
+                String str =st.nextToken();
+                if(str.charAt(0)=='\"' && str.charAt(str.length()-1)=='\"'){
+                    list.add(str);
+                }//check if "" are present
+                else{
+                    list.add("\""+str+"\"");
+                }
+                //not sure if " are req yet
+            }
+            res.add(list);
+        }
+        
+        return res;
+    }
+    
+}
---
+++ JDAS/src/Model.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.C63ACCD1-B180-39EE-C54B-B45A5A53D44D]
+// </editor-fold> 
+public class Model {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.959616C0-9A41-1A04-F0C7-62A382AC875A]
+    // </editor-fold> 
+    public Model () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.410FB467-F179-4E58-95DD-9C8641A0320F]
+    // </editor-fold> 
+    public boolean updatePublication (ArrayList<ArrayList<String>> list) {
+        return true;
+    }
+
+}
+
